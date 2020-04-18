2cdaa33d879ea753dc0e6835af8f24a51cea2fa6
Cody Mace <codymace@gmail.com>
2020-04-18 08:02:43 -0600

    Add springy arm.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,16 +6,45 @@ public class GameController : MonoBehaviour
 {
     public PlayerController player;
     public Transform moveToIndicator;
+    public Transform hand;
+    public Transform shoulder;
+    public Color armColor;
+
     public float planetRadius = 10;
+    public bool armOut = false;
+
+    LineRenderer armLine;
     // Start is called before the first frame update
     void Start()
     {
-        
+        armLine = hand.gameObject.AddComponent<LineRenderer>();
+        armLine.material = new Material(Shader.Find("Sprites/Default"));
+        armLine.widthMultiplier = 0.1f;
+        armLine.sortingOrder = hand.GetComponent<SpriteRenderer>().sortingOrder;
+        armLine.positionCount = 2;
+        armLine.startColor = armColor;
+        armLine.endColor = armColor;
+        armLine.numCapVertices = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0)) {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // hand.position = mousePos;
+            float dx = mousePos.x - hand.position.x;
+            float dy = mousePos.y - hand.position.y;
+            hand.GetComponent<Rigidbody2D>().MovePosition(mousePos);
+            hand.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            hand.GetComponent<SpringJoint2D>().dampingRatio = 0;
+            hand.GetComponent<SpringJoint2D>().frequency = 1;
+            armOut = true;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            // release arm
+            Invoke("SlowDownArm", 1.0f);
+        }
         if (Input.GetMouseButtonDown(1)) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             float angle = Mathf.Atan2(mousePos.y, mousePos.x);
@@ -23,6 +52,14 @@ public class GameController : MonoBehaviour
             moveToIndicator.position = new Vector2(Mathf.Cos(angle) * planetRadius, Mathf.Sin(angle) * planetRadius);
             moveToIndicator.GetComponent<Animator>().Play("MoveToIndicator");
         }
+        if (armOut) {
+            armLine.SetPositions(new Vector3[] {hand.position, shoulder.position} );
+        }
+    }
+
+    private void SlowDownArm() {
+        hand.GetComponent<SpringJoint2D>().dampingRatio = 0.8f;
+        hand.GetComponent<SpringJoint2D>().frequency = 1.5f;
     }
 
     public void StopIndicator() {
